f5e337fdf46156d139158cae65e30897f3c57f09
seaaaaaaa <paramee.seaza@gmail.com>
2026-01-26 21:54:20 +0700

    restaurant

--- triple_sum.cpp
+++ triple_sum.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+4 5
+-2 1 5 6
+1 3 5 6 7#include <algorithm>
 #include <set>
 using namespace std;
 
 bool check(int m,vector<int> v){
-  for(int i = 0; i < m; i++){
-    int n = v.size();
-    for(int i=0;i<n-2;i++){
-      int l=i+1;
-      int r=n-1;
-      while(l<r){
-        int res = v[i]+v[l]+v[r];
-        if(res<m) l++;
-        else if(res > m) r--;
-        else return true;
-      }
+  
+  int n = v.size();
+  for(int i=0;i<n-2;i++){
+    int l=i+1;
+    int r=n-1;
+    while(l<r){
+      int res = v[i]+v[l]+v[r];
+      if(res<m) l++;
+      else if(res > m) r--;
+      else return true;
     }
   }
   return false;
@@ -32,7 +33,7 @@ int main(){
   for(int i = 0; i < m; i++){
     int y;
     cin >> y;
-    if(check(y,v)) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    if(check(y,v)) cout << "YES" << endl;
+    else cout << "NO" << endl;
   }
 }
